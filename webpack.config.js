d47a3905fab03885928ecdd46e1da3dd1d590132
arthurtcheung <arthurtcheung>
2024-03-07 22:07:55 -0500

    webpack configured

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+// Modules
+const path = require('path');
+const dotenv = require('dotenv');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // generate an HTML file and inject the necessary script tags automatically
+dotenv.config();
+
+module.exports = {
+  entry: path.join(__dirname, './frontend/src/', 'index.js'),
+  output: {
+    filename: 'bundle.js', // NOTE Specified name 'bundle.js' for when webpack builds app and generates a bundle containing all modules and dependencies
+    path: path.resolve(__dirname, 'build'),
+  },
+  mode: process.env.MODE,
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }], '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.(css|scss)$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|JPG)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'CICDEEZ',
+      template: path.join(__dirname, './frontend/public', 'index.html'),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/build'),
+      publicPath: '/',
+    },
+    hot: true, // enables Hot Module Replacement
+    historyApiFallback: true,
+    compress: true,
+    port: process.env.DEV_PORT,
+    proxy: [
+      {
+        '/api': {
+          target: `http://localhost:${process.env.SERV_PORT}`,
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  performance: {
+    hints: false, // webpack won't emit warnings based on these size limits during the build
+    maxEntrypointSize: 512000, // sets the max size, in bytes, that the entry point (main bundle) file should be before triggering a warning or error
+    maxAssetSize: 512000, // sets the maximum size for individual assets (like images, stylesheets, etc.)
+  },
+};
