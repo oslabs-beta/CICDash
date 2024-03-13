be3d8538dcba0220f75b3c23bf1f66766e184b20
gzeal <unovan@protonmail.com>
2024-03-12 23:22:59 -0400

    Creating a react frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <>
+      <p>This is a react element</p>
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <>
+      <body>
+        <h1>hello world!</h1>
+        <p>Authorize your app with:</p>
+        <a href='https://github.com/login/oauth/authorize?client_id=Iv1.cdabdacb14c5030c'>
+          Login with GitHub
+        </a>
+      </body>
+    </>
+  );
+};
+
+export default Home;
