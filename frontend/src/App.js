0094d2c3b7008aa1a40ccb07cb79a685da91e334
Russell Apolonio <russell1740@gmail.com>
2024-03-13 17:43:03 -0700

    Centered login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Home from './pages/Home';
+import './stylesheet.css';
 
 function App() {
   return (
     <>
-      <p>This is a react element</p>
       <Home />
     </>
   );
