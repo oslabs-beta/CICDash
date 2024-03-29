d008faf5671d6b5f231735d920458c963cce1cfe
j-s-n <jasonkuroda@gmail.com>
2024-03-29 12:09:24 -0400

    updated mongo model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,17 +1,52 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-require('dotenv').config();
+//nested schemas to arrange by propeties. needs to be in reverse order so the schema can be referenced by the following schema.
 
-const Schema = mongoose.Schema;
+const stepSchema = new Schema({
+  step_name: { type: String, required: true },
+  status: { type: String, required: true },
+  conclusion: { type: String, required: true },
+  number: { type: Number, required: true },
+  created_at: { type: Date, required: true },
+  started_at: { type: Date, required: true },
+  completed_at: { type: Date, required: true },
+});
+
+const metricSchema = new Schema({
+  head_branch: { type: String, required: true },
+  status: { type: String, required: true },
+  conclusion: { type: String, required: true },
+  created_at: { type: Date, required: true },
+  started_at: { type: Date, required: true },
+  completed_at: { type: Date, required: true },
+  steps: [stepSchema],
+});
+
+const jobSchema = new Schema({
+  job_id: { type: String, required: true },
+  metrics: [metricSchema],
+});
+
+const workflowSchema = new Schema({
+  workflow: { type: String, required: true },
+  jobs: [jobSchema],
+});
+
+const repoSchema = new Schema({
+  repo: { type: String, required: true },
+  workflows: [workflowSchema],
+});
+
+const ownerSchema = new Schema({
+  owner: { type: String, required: true },
+  repos: [repoSchema],
+});
 
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
-  access_token: { type: String, required: true },
   refresh_token: { type: String, required: true },
-  // repo: { type: String, required: true },
-  // workflow_id: { type: String, required: true },
-  // email: { type: String, required: true },
-  jobs: { type: Array },
+  owner: [ownerSchema],
 });
 
 const User = mongoose.model('user', userSchema);
